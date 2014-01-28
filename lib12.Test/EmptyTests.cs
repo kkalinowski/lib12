01e03b0723c05b26468df90c3d1ec1aa0427ef6f
Krzysztof Kalinowski <kkalinowski@outlook.com>
2014-01-28 14:27:49 +0100

    Added Empty class. Added fluent assertions

---
+++ lib12.Test/EmptyTests.cs
@@ -0,0 +1,17 @@
+﻿using FluentAssertions;
+using lib12.Core;
+using Xunit;
+
+namespace lib12.Test
+{
+    public sealed class EmptyTests
+    {
+        [Fact]
+        public void empty_array_returns_empty_array()
+        {
+            var array = Empty.Array<int>();
+
+            array.Should().NotBeNull().And.BeEmpty();
+        }
+    }
+}
---
+++ lib12/Core/Empty.cs
@@ -0,0 +1,18 @@
+﻿namespace lib12.Core
+{
+    /// <summary>
+    /// Handles empty objects creation and validation
+    /// </summary>
+    public static class Empty
+    {
+        /// <summary>
+        /// Returns empty array of given type
+        /// </summary>
+        /// <typeparam name="T">Type of array</typeparam>
+        /// <returns></returns>
+        public static T[] Array<T>()
+        {
+            return new T[0];
+        }
+    }
+}
