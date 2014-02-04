e47403b0cbe8723f8ef20a5019e3c26064a4e76e
Krzysztof Kalinowski <kkalinowski@outlook.com>
2014-02-04 13:40:55 +0100

    Added ObjectExtensions

---
+++ lib12.Test/ExtensionTests/ObjectTests.cs
@@ -0,0 +1,77 @@
+﻿using FluentAssertions;
+using lib12.Extensions;
+using System;
+using Xunit;
+
+namespace lib12.Test.ExtensionTests
+{
+    public class ObjectTests
+    {
+        private const string nullObject = null;
+        private const string notNullObject = "test_string";
+        private const string StringPropValue = "test_string_prop";
+        private readonly NullCheck notNullComplexObject = new NullCheck { StringProp = StringPropValue };
+        private readonly NullCheck nullComplexObject = null;
+
+        private class NullCheck
+        {
+            public string StringProp { get; set; }
+            public int IntProp { get; set; }
+        }
+
+        [Fact]
+        public void null_returns_true_if_object_is_null()
+        {
+            Assert.True(nullObject.Null());
+        }
+
+        [Fact]
+        public void null_returns_false_if_object_is_not_null()
+        {
+            Assert.False(notNullObject.Null());
+        }
+
+        [Fact]
+        public void throw_exception_throws_exception_if_object_is_null()
+        {
+            nullObject.Invoking(x => x.Null().ThrowException()).ShouldThrow<NullReferenceException>();
+        }
+
+        [Fact]
+        public void throw_exception_doesnt_throws_exception_if_object_is_not_null()
+        {
+            notNullObject.Invoking(x => x.Null().ThrowException()).ShouldNotThrow<NullReferenceException>();
+        }
+
+        [Fact]
+        public void not_null_returns_false_if_object_is_null()
+        {
+            Assert.False(nullObject.NotNull());
+        }
+
+        [Fact]
+        public void not_null_returns_true_if_object_is_not_null()
+        {
+            Assert.True(notNullObject.NotNull());
+        }
+
+        [Fact]
+        public void get_value_or_default_returns_value_if_object_is_not_null()
+        {
+            notNullComplexObject.NotNull().Get(x => x.StringProp).Should().Be(StringPropValue);
+        }
+
+        [Fact]
+        public void get_value_or_default_returns_default_if_object_is_null()
+        {
+            var result = nullComplexObject.NotNull().Get(x => x.IntProp).Should().Be(default(int));
+        }
+
+        [Fact]
+        public void get_value_or_default_returns_given_default_if_object_is_null()
+        {
+            const string defaultString = "default_string";
+            nullComplexObject.NotNull().Get(x => x.StringProp, defaultString).Should().Be(defaultString);
+        }
+    }
+}
---
+++ lib12/Extensions/ObjectExtension.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Linq.Expressions;
+
+namespace lib12.Extensions
+{
+    public static class ObjectExtension
+    {
+        public class NullResult
+        {
+            private readonly bool result;
+
+            public NullResult(bool result)
+            {
+                this.result = result;
+            }
+
+            public static implicit operator bool(NullResult nullResult)
+            {
+                return nullResult.result;
+            }
+
+            /// <summary>
+            /// Throw exception if checked object is null
+            /// </summary>
+            public void ThrowException()
+            {
+                if (result)
+                    throw new NullReferenceException();
+            }
+        }
+
+        public class NotNullResult<TObject> where TObject : class
+        {
+            private readonly bool result;
+            private readonly TObject self;
+
+            public NotNullResult(TObject self)
+            {
+                this.self = self;
+                result = self != null;
+            }
+
+            public static implicit operator bool(NotNullResult<TObject> notNullResult)
+            {
+                return notNullResult.result;
+            }
+
+            public TValue Get<TValue>(Expression<Func<TObject, TValue>> expression, TValue defaultValue = default(TValue))
+            {
+                if (result)
+                    return expression.Compile().Invoke(self);
+                else
+                    return defaultValue;
+            }
+        }
+
+        /// <summary>
+        /// Check if given object is null
+        /// </summary>
+        /// <param name="self"></param>
+        /// <returns></returns>
+        public static NullResult Null(this object self)
+        {
+            return new NullResult(self == null);
+        }
+
+        /// <summary>
+        /// Check if given object is not null
+        /// </summary>
+        /// <param name="self"></param>
+        /// <returns></returns>
+        public static NotNullResult<TObject> NotNull<TObject>(this TObject self) where TObject : class
+        {
+            return new NotNullResult<TObject>(self);
+        }
+    }
+}
