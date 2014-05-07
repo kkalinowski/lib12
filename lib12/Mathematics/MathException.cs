fbfe59478fce00d600ca1747b6044f62d500e2b8
Krzysztof Kalinowski <kkalinowski@outlook.com>
2014-05-07 23:01:42 +0200

    Added quadratic equation computation

---
+++ lib12/Mathematics/MathException.cs
@@ -0,0 +1,13 @@
+﻿using lib12.Exceptions;
+
+namespace lib12.Mathematics
+{
+    public class MathException : lib12Exception
+    {
+        public MathException(string message)
+            : base(message)
+        {
+
+        }
+    }
+}
---
+++ lib12/Mathematics/QuadraticEquationResultType.cs
@@ -0,0 +1,21 @@
+﻿namespace lib12.Mathematics
+{
+    /// <summary>
+    /// Describes type of quadratic equation result
+    /// </summary>
+    public enum QuadraticEquationResultType
+    {
+        /// <summary>
+        /// The equation has no results
+        /// </summary>
+        NoResults,
+        /// <summary>
+        /// The equation has ono result
+        /// </summary>
+        OneResult,
+        /// <summary>
+        /// The equation has two results
+        /// </summary>
+        TwoResults
+    }
+}
