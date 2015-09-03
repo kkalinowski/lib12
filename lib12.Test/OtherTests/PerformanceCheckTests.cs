d5253956b10080e4f77cda76cd1a55eea8d23301
“Krzysztof <krzysztof.kalinowski@sabre.com>
2015-09-03 09:19:52 +0200

    Added PerformanceCheck and tests for it

---
+++ lib12.Test/OtherTests/PerformanceCheckTests.cs
@@ -0,0 +1,31 @@
+﻿using System.Threading;
+using lib12.Misc;
+using Should;
+using Xunit;
+
+namespace lib12.Test.OtherTests
+{
+    public class PerformanceCheckTests
+    {
+        private void ToCheck()
+        {
+            Thread.Sleep(100);
+        }
+
+        [Fact]
+        public void counter_is_working()
+        {
+            PerformanceCheck
+                .Check(ToCheck)
+                .ShouldBeGreaterThan(0);
+        }
+
+        [Fact]
+        public void counter_is_working_for_lambda_expression()
+        {
+            PerformanceCheck
+                .Check(() => Thread.Sleep(100))
+                .ShouldBeGreaterThan(0);
+        }
+    }
+}
