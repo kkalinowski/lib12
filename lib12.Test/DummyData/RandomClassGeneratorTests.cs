d1735207c4e8186bb4318ebc0d751b2cc5075210
kkalinowski@BASE <kkalinowski@outlook.com>
2015-11-25 15:43:14 +0100

    Random data generator is now static

--- lib12.Test/DummyData/RandomClassGeneratorTests.cs
+++ lib12.Test/DummyData/RandomClassGeneratorTests.cs
@@ -12,22 +12,11 @@ namespace lib12.Test.DummyData
         private const int CollectionSize = 12;
         #endregion
 
-        #region Fields
-        private readonly RandomClassGenerator generator;
-        #endregion
-
-        #region ctor
-        public RandomClassGeneratorTests()
-        {
-            generator = new RandomClassGenerator();
-        }
-        #endregion
-
         [Fact]
         public void Generate_returns_not_null_items_of_correct_type()
         {
-            var generated = generator.Generate<Account>(CollectionSize);
-            generated.Count.ShouldEqual(CollectionSize);
+            var generated = DummyClass.Generate<Account>(CollectionSize);
+            generated.Count().ShouldEqual(CollectionSize);
 
             foreach (var item in generated)
             {
@@ -41,7 +30,7 @@ namespace lib12.Test.DummyData
         {
             const int minLength = 3;
             const int maxLength = 7;
-            var generated = generator.Generate(CollectionSize, new StringGenerator<ClassToGenerate>(x => x.Text, minLength, maxLength));
+            var generated = DummyClass.Generate(CollectionSize, new StringGenerator<ClassToGenerate>(x => x.Text, minLength, maxLength));
 
             foreach (var item in generated)
             {
@@ -53,7 +42,7 @@ namespace lib12.Test.DummyData
         [Fact]
         public void enum_generation_test()
         {
-            var generated = generator.Generate(CollectionSize, new EnumGenerator<ClassToGenerate, ClassToGenerate.EnumToGenerate>(x => x.Enum));
+            var generated = DummyClass.Generate(CollectionSize, new EnumGenerator<ClassToGenerate, ClassToGenerate.EnumToGenerate>(x => x.Enum));
 
             foreach (var item in generated)
             {
@@ -64,7 +53,7 @@ namespace lib12.Test.DummyData
         [Fact]
         public void bool_generation_test()
         {
-            var generated = generator.Generate(CollectionSize, new BoolGenerator<ClassToGenerate>(x => x.Bool));
+            var generated = DummyClass.Generate(CollectionSize, new BoolGenerator<ClassToGenerate>(x => x.Bool));
 
             foreach (var item in generated)
             {
@@ -75,7 +64,7 @@ namespace lib12.Test.DummyData
         [Fact]
         public void int_generation_test()
         {
-            var generated = generator.Generate(CollectionSize, new IntGenerator<ClassToGenerate>(x => x.Int, 50, 100));
+            var generated = DummyClass.Generate(CollectionSize, new IntGenerator<ClassToGenerate>(x => x.Int, 50, 100));
 
             foreach (var item in generated)
             {
@@ -87,7 +76,7 @@ namespace lib12.Test.DummyData
         [Fact]
         public void double_generation_test()
         {
-            var generated = generator.Generate(CollectionSize, new DoubleGenerator<ClassToGenerate>(x => x.Double, 70, 120));
+            var generated = DummyClass.Generate(CollectionSize, new DoubleGenerator<ClassToGenerate>(x => x.Double, 70, 120));
 
             foreach (var item in generated)
             {
@@ -99,7 +88,7 @@ namespace lib12.Test.DummyData
         [Fact]
         public void creation_of_complex_class_test()
         {
-            var generated = generator.Generate<Account>(CollectionSize);
+            var generated = DummyClass.Generate<Account>(CollectionSize);
 
             foreach (var item in generated)
             {
@@ -119,7 +108,7 @@ namespace lib12.Test.DummyData
         public void available_values_generator()
         {
             var names = new[] { "name1", "name2", "name3" };
-            var generated = generator.Generate(CollectionSize, new AvailableValuesGenerator<Account, string>(x => x.Name, names));
+            var generated = DummyClass.Generate(CollectionSize, new AvailableValuesGenerator<Account, string>(x => x.Name, names));
 
             foreach (var item in generated)
             {
