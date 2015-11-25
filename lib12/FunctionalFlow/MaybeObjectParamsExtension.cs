0338972533aedc9e3f4df30ad21653b05c0d21d2
kkalinowski@BASE <kkalinowski@outlook.com>
2015-11-25 14:54:07 +0100

    Maybe removed

--- lib12/FunctionalFlow/MaybeObjectParamsExtension.cs
+++ lib12/FunctionalFlow/MaybeObjectParamsExtension.cs
@@ -1,27 +1,28 @@
 ﻿using System.Linq;
+using lib12.Extensions;
 
 namespace lib12.FunctionalFlow
 {
     public static class MaybeObjectParamsExtension
     {
-        public static Maybe AllNull(params object[] toCheck)
+        public static bool AllNull(params object[] toCheck)
         {
-            return Maybe.Create(toCheck.All(x => x.Null()));
+            return toCheck.All(x => x.Null());
         }
 
-        public static Maybe AllNotNull(params object[] toCheck)
+        public static bool AllNotNull(params object[] toCheck)
         {
-            return Maybe.Create(toCheck.All(x => x.NotNull()));
+            return toCheck.All(x => x.NotNull());
         }
 
-        public static Maybe AnyNull(params object[] toCheck)
+        public static bool AnyNull(params object[] toCheck)
         {
-            return Maybe.Create(toCheck.Any(x => x.Null()));
+            return toCheck.Any(x => x.Null());
         }
 
-        public static Maybe AnyNotNull(params object[] toCheck)
+        public static bool AnyNotNull(params object[] toCheck)
         {
-            return Maybe.Create(toCheck.Any(x => x.NotNull()));
+            return toCheck.Any(x => x.NotNull());
         }
     }
 }
