3d5da46f3cff83b756d1d2f640e37f3c06c16806
Kalinowski, Krzysztof <krzysztof.kalinowski@sabre.com>
2015-12-09 13:51:24 +0100

    Added XmlExtensions and tests

---
+++ lib12.Test/DummyData/XmlExtensionsTest.cs
@@ -0,0 +1,46 @@
+﻿using System.IO;
+using System.Linq;
+using System.Xml.Linq;
+using System.Xml.XPath;
+using lib12.Data.Xml;
+using Should;
+using Xunit;
+
+namespace lib12.Test.DummyData
+{
+    public class XmlExtensionsTests
+    {
+        private readonly string sampleXml = File.ReadAllText("_Files/Sample.xml");
+
+        [Fact]
+        public void AddChild_CorrectInformation_CorrectResult()
+        {
+            const string childName = "_child_";
+            var xdoc = XDocument.Parse(sampleXml);
+            var element = xdoc.XPathSelectElement("breakfast_menu/food[1]");
+
+            element.Descendants().Count().ShouldEqual(4);
+            var returnedChild = element.AddChild(childName);
+            element.Descendants().Count().ShouldEqual(5);
+
+            var child = xdoc.XPathSelectElement("breakfast_menu/food/" + childName);
+            child.ShouldNotBeNull();
+            child.ShouldEqual(returnedChild);
+        }
+
+        [Fact]
+        public void AddAttribute_CorrectInformation_CorrectResult()
+        {
+            const string attributeName = "_attribute_";
+            const string attributeValue = "_value_";
+            var xdoc = XDocument.Parse(sampleXml);
+            var element = xdoc.XPathSelectElement("breakfast_menu/food[1]");
+
+            element.Attributes().ShouldBeEmpty();
+            var returnedElement = element.AddAttribute(attributeName, attributeValue);
+            returnedElement.ShouldEqual(element);
+            element.Attributes().Count().ShouldEqual(1);
+            element.Attribute(attributeName).Value.ShouldEqual(attributeValue);
+        }
+    }
+}
---
+++ lib12/Data/Xml/XmlExtensions.cs
@@ -0,0 +1,23 @@
+﻿using System.Xml.Linq;
+
+namespace lib12.Data.Xml
+{
+    public static class XmlExtensions
+    {
+        public static XElement AddChild(this XElement parent, string name)
+        {
+            var element = new XElement(name);
+            parent.Add(element);
+
+            return element;
+        }
+
+        public static XElement AddAttribute(this XElement element, string name, object value)
+        {
+            var attribute = new XAttribute(name, value);
+            element.Add(attribute);
+
+            return element;
+        }
+    }
+}
