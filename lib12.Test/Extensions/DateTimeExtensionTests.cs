c2e8c536252ee198edf18e0796d4be078bf69b8d
kkalinowski@BASE <kkalinowski@outlook.com>
2016-05-17 19:47:08 +0200

    Added DateTime.IsEmpty

--- lib12.Test/Extensions/DateTimeExtensionTests.cs
+++ lib12.Test/Extensions/DateTimeExtensionTests.cs
@@ -21,5 +21,17 @@ namespace lib12.Test.Extensions
             const long toParse = 1391083200;
             DateTimeExtension.ParseUnixTimeStamp(toParse).ShouldEqual(new DateTime(2014, 01, 30, 12, 0, 0, 0, DateTimeKind.Utc));
         }
+
+        [Fact]
+        public void isempty_returns_true_on_empty_datetime()
+        {
+            new DateTime().IsEmpty().ShouldBeTrue();
+        }
+
+        [Fact]
+        public void isempty_returns_false_on_non_empty_datetime()
+        {
+            new DateTime(1900, 3, 25).IsEmpty().ShouldBeFalse();
+        }
     }
 }
