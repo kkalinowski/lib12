e004c29b57f2a02b86c23dd0576d1baf394d641c
Krzysztof Kalinowski <kkalinowski@outlook.com>
2017-08-28 22:36:06 +0200

    Added CollectionFactory

---
+++ lib12/Collections/CollectionFactory.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace lib12.Collections
+{
+    public static class CollectionFactory
+    {
+        public static IEnumerable<TResult> CreateEnumerable<TResult, TStep>(Func<TStep, TResult> func, Func<TStep, TStep> stepFunc, TStep initialStep)
+        {
+            var currentStep = initialStep;
+            while (true)
+            {
+                yield return func(currentStep);
+                currentStep = stepFunc(currentStep);
+            }
+        }
+    }
+}
