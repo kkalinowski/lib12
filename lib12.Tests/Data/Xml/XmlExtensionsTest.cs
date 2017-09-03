a3e109210acfeb8b10053099895fc34df3089af8
Krzysztof Kalinowski <kkalinowski@outlook.com>
2017-09-03 20:27:19 +0200

    Fixed XmlExtensionsTest

--- lib12.Tests/Data/Xml/XmlExtensionsTest.cs
+++ lib12.Tests/Data/Xml/XmlExtensionsTest.cs
@@ -1,4 +1,9 @@
 ﻿using System.IO;
+using System.Linq;
+using System.Xml.Linq;
+using System.Xml.XPath;
+using lib12.Data.Xml;
+using Shouldly;
 using Xunit;
 
 namespace lib12.Tests.Data.Xml
@@ -8,34 +13,34 @@ namespace lib12.Tests.Data.Xml
         private readonly string sampleXml = File.ReadAllText("_Files/Sample.xml");
 
         [Fact]
-        public void AddChild_CorrectInformation_CorrectResult()
+        public void addchild_happy_path()
         {
-            //const string childName = "_child_";
-            //var xdoc = XDocument.Parse(sampleXml);
-            //var element = xdoc.XPathSelectElement("breakfast_menu/food[1]");
+            const string childName = "_child_";
+            var xdoc = XDocument.Parse(sampleXml);
+            var element = xdoc.XPathSelectElement("breakfast_menu/food[1]");
 
-            //element.Descendants().Count().ShouldBe(4);
-            //var returnedChild = element.AddChild(childName);
-            //element.Descendants().Count().ShouldBe(5);
+            element.Descendants().Count().ShouldBe(4);
+            var returnedChild = element.AddChild(childName);
+            element.Descendants().Count().ShouldBe(5);
 
-            //var child = xdoc.XPathSelectElement("breakfast_menu/food/" + childName);
-            //child.ShouldNotBeNull();
-            //child.ShouldBe(returnedChild);
+            var child = xdoc.XPathSelectElement("breakfast_menu/food/" + childName);
+            child.ShouldNotBeNull();
+            child.ShouldBe(returnedChild);
         }
 
         [Fact]
-        public void AddAttribute_CorrectInformation_CorrectResult()
+        public void addattribute_happy_path()
         {
-            //const string attributeName = "_attribute_";
-            //const string attributeValue = "_value_";
-            //var xdoc = XDocument.Parse(sampleXml);
-            //var element = xdoc.XPathSelectElement("breakfast_menu/food[1]");
+            const string attributeName = "_attribute_";
+            const string attributeValue = "_value_";
+            var xdoc = XDocument.Parse(sampleXml);
+            var element = xdoc.XPathSelectElement("breakfast_menu/food[1]");
 
-            //element.Attributes().ShouldBeEmpty();
-            //var returnedElement = element.AddAttribute(attributeName, attributeValue);
-            //returnedElement.ShouldBe(element);
-            //element.Attributes().Count().ShouldBe(1);
-            //element.Attribute(attributeName).Value.ShouldBe(attributeValue);
+            element.Attributes().ShouldBeEmpty();
+            var returnedElement = element.AddAttribute(attributeName, attributeValue);
+            returnedElement.ShouldBe(element);
+            element.Attributes().Count().ShouldBe(1);
+            element.Attribute(attributeName).Value.ShouldBe(attributeValue);
         }
     }
 }
