1d7775b3741bfcad9793b8a6efa16653fdf7c993
Krzysztof Kalinowski <krzysztof.kalinowski@makingwaves.com>
2017-09-04 09:26:48 +0200

    Added Truncate and tests for StringExtensions

--- lib12.Tests/Extensions/StringExtensionTests.cs
+++ lib12.Tests/Extensions/StringExtensionTests.cs
@@ -1,4 +1,5 @@
-﻿using lib12.Extensions;
+﻿using System;
+using lib12.Extensions;
 using Shouldly;
 using Xunit;
 
@@ -44,6 +45,68 @@ namespace lib12.Tests.Extensions
             const string second = "Different test string";
 
             first.EqualsIgnoreCase(second).ShouldBeFalse();
+        }
+
+        [Fact]
+        public void equalsmatchcase_two_equal_same_case_returns_true()
+        {
+            const string first = "Test string";
+            const string second = "Test string";
+
+            first.EqualsMatchCase(second).ShouldBeTrue();
+        }
+
+        [Fact]
+        public void equalsmatchcase_two_equal_different_case_returns_flase()
+        {
+            const string first = "Test string";
+            const string second = "tEst sTrinG";
+
+            first.EqualsMatchCase(second).ShouldBeFalse();
+        }
+
+        [Fact]
+        public void equalsmatchcase_two_different_returns_false()
+        {
+            const string first = "Test string";
+            const string second = "Different test string";
+
+            first.EqualsMatchCase(second).ShouldBeFalse();
+        }
+
+        [Fact]
+        public void truncate_throws_exception_if_max_length_is_equal_or_less_than_zero()
+        {
+            const string text = "text5";
+            Assert.Throws<ArgumentOutOfRangeException>(() => text.Truncate(0).ShouldBe(text));
+        }
+
+        [Fact]
+        public void truncate_on_null_returns_empty_string()
+        {
+            string text = null;
+            text.Truncate(12).ShouldBeEmpty();
+        }
+
+        [Fact]
+        public void truncate_on_empty_string_returns_empty_string()
+        {
+            var text = string.Empty;
+            text.Truncate(12).ShouldBeEmpty();
+        }
+
+        [Fact]
+        public void truncate_on_string_with_length_lower_than_max_returns_same_string()
+        {
+            const string text = "text5";
+            text.Truncate(12).ShouldBe(text);
+        }
+
+        [Fact]
+        public void truncate_on_string_with_length_bigger_than_max_returns_truncated_string()
+        {
+            const string text = "text5";
+            text.Truncate(2).ShouldBe("te");
         }
     }
 }
--- lib12/Extensions/StringExtension.cs
+++ lib12/Extensions/StringExtension.cs
@@ -16,6 +16,17 @@ namespace lib12.Extensions
         }
 
         /// <summary>
+        /// Check if strings are equal with matching case
+        /// </summary>
+        /// <param name="target"></param>
+        /// <param name="toCompare"></param>
+        /// <returns></returns>
+        public static bool EqualsMatchCase(this string target, string toCompare)
+        {
+            return string.Equals(target, toCompare, StringComparison.Ordinal);
+        }
+
+        /// <summary>
         /// Returns empty string if given string is null
         /// </summary>
         /// <param name="target"></param>
@@ -25,14 +36,40 @@ namespace lib12.Extensions
             return target ?? string.Empty;
         }
 
+        /// <summary>
+        /// Indicates whether given string is null or empty
+        /// </summary>
+        /// <param name="target"></param>
+        /// <returns></returns>
         public static bool IsNullOrEmpty(this string target)
         {
             return string.IsNullOrEmpty(target);
         }
 
+        /// <summary>
+        /// Indicates whether given string is not null and not empty
+        /// </summary>
+        /// <param name="target"></param>
+        /// <returns></returns>
         public static bool IsNotNullAndNotEmpty(this string target)
         {
             return !string.IsNullOrEmpty(target);
+        }
+
+        /// <summary>
+        /// Truncates string to given length
+        /// </summary>
+        /// <param name="text"></param>
+        /// <param name="maxLength">Maximum length of given string</param>
+        /// <exception cref="ArgumentOutOfRangeException">maxLength - Truncated string must have at least 1 length</exception>
+        /// <returns></returns>
+        public static string Truncate(this string text, int maxLength)
+        {
+            if (maxLength <= 0)
+                throw new ArgumentOutOfRangeException(nameof(maxLength), "Truncated string must have at least 1 length");
+
+            var toTruncate = text.Recover();
+            return toTruncate.Length <= maxLength ? toTruncate : toTruncate.Substring(0, maxLength);
         }
     }
 }
