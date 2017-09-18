1e6e514c1cea27122c8ebd8ef9237f7e760afb19
Krzysztof Kalinowski <krzysztof.kalinowski@makingwaves.com>
2017-09-18 09:48:03 +0200

    Added CheckTests

---
+++ lib12.Tests/Checking/CheckTests.cs
@@ -0,0 +1,41 @@
+﻿using lib12.Checking;
+using Shouldly;
+using Xunit;
+
+namespace lib12.Tests.Checking
+{
+    public class CheckTests
+    {
+        [Fact]
+        public void all_are_null_test()
+        {
+            Check.AllAreNull(null, null).ShouldBeTrue();
+            Check.AllAreNull(new object(), null).ShouldBeFalse();
+            Check.AllAreNull(new object(), new object()).ShouldBeFalse();
+        }
+
+        [Fact]
+        public void all_are_not_null_test()
+        {
+            Check.AllAreNotNull(null, null).ShouldBeFalse();
+            Check.AllAreNotNull(new object(), null).ShouldBeFalse();
+            Check.AllAreNotNull(new object(), new object()).ShouldBeTrue();
+        }
+
+        [Fact]
+        public void any_is_null_test()
+        {
+            Check.AnyIsNull(null, null).ShouldBeTrue();
+            Check.AnyIsNull(new object(), null).ShouldBeTrue();
+            Check.AnyIsNull(new object(), new object()).ShouldBeFalse();
+        }
+
+        [Fact]
+        public void any_is_not_null_test()
+        {
+            Check.AnyIsNotNull(null, null).ShouldBeFalse();
+            Check.AnyIsNotNull(new object(), null).ShouldBeTrue();
+            Check.AnyIsNotNull(new object(), new object()).ShouldBeTrue();
+        }
+    }
+}
