82a6e67685f89480189a4dc773af873e0c885c08
Krzysztof Kalinowski <kkalinowski@outlook.com>
2018-04-18 21:12:02 +0200

    Added ToBool extension

---
+++ lib12.Tests/Mathematics/IntExtensionTests.cs
@@ -0,0 +1,21 @@
+﻿using lib12.Mathematics.Extensions;
+using Shouldly;
+using Xunit;
+
+namespace lib12.Tests.Mathematics
+{
+    public class IntExtensionTests
+    {
+        [Fact]
+        public void to_bool_returns_false_if_given_zero()
+        {
+            0.ToBool().ShouldBeFalse();
+        }
+
+        [Fact]
+        public void to_bool_returns_true_if_given_non_zero()
+        {
+            12.ToBool().ShouldBeTrue();
+        }
+    }
+}
---
+++ lib12/Mathematics/Extensions/IntExtension.cs
@@ -0,0 +1,15 @@
+﻿namespace lib12.Mathematics.Extensions
+{
+    public static class IntExtension
+    {
+        /// <summary>
+        /// Converts int to bool
+        /// </summary>
+        /// <param name="source">The source int</param>
+        /// <returns>True if not zero</returns>
+        public static bool ToBool(this int source)
+        {
+            return source != 0;
+        }
+    }
+}
