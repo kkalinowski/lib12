a4e6fdb321eda93042810240161784c787a7462d
Krzysztof Kalinowski <kkalinowski@outlook.com>
2018-09-20 22:28:43 +0200

    Started working on paging

---
+++ lib12.Tests/Collections/Paging/PagingTests.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections.Generic;
+using lib12.Collections;
+using lib12.Collections.Paging;
+using lib12.Data.Random;
+using Shouldly;
+using Xunit;
+
+namespace lib12.Tests.Collections.Paging
+{
+    public class PagingTests
+    {
+        [Fact]
+        public void get_number_of_pages_returns_zero_for_null()
+        {
+            ((IEnumerable<int>)null).GetNumberOfPages(10).ShouldBe(0);
+        }
+
+        [Fact]
+        public void get_number_of_pages_returns_zero_for_empty_collection()
+        {
+            Empty.Enumerable<int>().GetNumberOfPages(10).ShouldBe(0);
+        }
+
+        [Fact]
+        public void get_number_of_pages_throws_exception_if_number_of_items_per_page_is_less_than_one()
+        {
+            var collection = CollectionFactory.CreateArray(10, x => Rand.NextInt());
+            Assert.Throws<lib12Exception>(() => collection.GetNumberOfPages(0));
+        }
+
+        [Fact]
+        public void get_number_of_pages_happy_path()
+        {
+            var collection = CollectionFactory.CreateArray(10, x => Rand.NextInt());
+            collection.GetNumberOfPages(4).ShouldBe(3);
+        }
+    }
+}
---
+++ lib12/Collections/Paging/PagingExtension.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace lib12.Collections.Paging
+{
+    public static class PagingExtension
+    {
+        public static IEnumerable<T> GetPage<T>(this IEnumerable<T> source, int page, int itemsPerPage)
+        {
+            return null;
+        }
+
+        public static int GetNumberOfPages<T>(this IEnumerable<T> source, int itemsPerPage)
+        {
+            var itemsCount = (double)source.Recover().Count();
+            return (int)Math.Ceiling(itemsCount / itemsPerPage);
+        }
+    }
+}
