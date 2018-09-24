1797cafe88069807a0c1a8f6deb287f284e2811e
Krzysztof Kalinowski <krzysztof.kalinowski@makingwaves.com>
2018-09-24 12:02:52 +0200

    Changes in Paging - more methods available for end user

--- lib12/Collections/Paging/PagingExtension.cs
+++ lib12/Collections/Paging/PagingExtension.cs
@@ -18,18 +18,23 @@ namespace lib12.Collections.Paging
             if(pageNumber > numberOfPages && source.IsNotNullAndNotEmpty())
                 throw new lib12Exception($"Page numbers can't be higher than page count. At this moment page number is {pageNumber}, when number of pages is {numberOfPages}");
 
-            var pageItems = source
+            var pageItems = GetPageItems(source, pageNumber, itemsPerPage);
+            return new PagingResult<T>(pageNumber, itemsPerPage, numberOfPages, pageItems);
+        }
+
+        public static int GetNumberOfPages<T>(IEnumerable<T> source, int itemsPerPage)
+        {
+            var itemsCount = (double)source.Recover().Count();
+            return (int)Math.Ceiling(itemsCount / itemsPerPage);
+        }
+
+        public static T[] GetPageItems<T>(IEnumerable<T> source, int pageNumber, int itemsPerPage)
+        {
+            return source
                 .Recover()
                 .Skip(itemsPerPage * (pageNumber - 1))
                 .Take(itemsPerPage)
                 .ToArray();
-            return new PagingResult<T>(pageNumber, itemsPerPage, numberOfPages, pageItems);
-        }
-
-        private static int GetNumberOfPages<T>(IEnumerable<T> source, int itemsPerPage)
-        {
-            var itemsCount = (double)source.Recover().Count();
-            return (int)Math.Ceiling(itemsCount / itemsPerPage);
         }
     }
 }
