ea8a2140b00f9db225bad41462d5cdc6d1518780
Krzysztof Kalinowski <kkalinowski@outlook.com>
2018-10-02 21:12:07 +0200

    Added unit tests for Packing

---
+++ lib12.Tests/Collections/Packing/PackTests.cs
@@ -0,0 +1,20 @@
+﻿using lib12.Collections.Packing;
+using Shouldly;
+using Xunit;
+
+namespace lib12.Tests.Collections.Packing
+{
+    public class PackTests
+    {
+        [Fact]
+        public void PackIntoArray_is_correct_for_value_types()
+        {
+            var result = Pack.IntoArray(12, 100, 3);
+
+            result.Length.ShouldBe(3);
+            result[0].ShouldBe(12);
+            result[1].ShouldBe(100);
+            result[2].ShouldBe(3);
+        }
+    }
+}
