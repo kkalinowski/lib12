ad81811d1348ad6e30b59b3389efc89a08a0c82f
Krzysztof Kalinowski <kkalinowski@outlook.com>
2018-10-16 21:56:05 +0200

    IListExtension changed to ICollectionExtension. Added RemoveBy method.

---
+++ lib12/Collections/ICollectionExtension.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace lib12.Collections
+{
+    /// <summary>
+    /// ICollectionExtension
+    /// </summary>
+    public static class ICollectionExtension
+    {
+        /// <summary>
+        /// Removes from collection elements from other collection
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="collection">The collection to remove elements from</param>
+        /// <param name="toRemove">Elements to remove</param>
+        public static void RemoveRange<T>(this ICollection<T> collection, IEnumerable<T> toRemove)
+        {
+            toRemove.ForEach(x => collection.Remove(x));
+        }
+
+        /// <summary>
+        /// Removes items from collection based on given predicate
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="collection">The collection to remove elements from</param>
+        /// <param name="predicate">The predicate if item needs to be removed</param>
+        public static void RemoveBy<T>(this ICollection<T> collection, Predicate<T> predicate)
+        {
+            foreach (var item in collection.Recover().ToArray())
+            {
+                if (predicate(item))
+                    collection.Remove(item);
+            }
+        }
+    }
+}
