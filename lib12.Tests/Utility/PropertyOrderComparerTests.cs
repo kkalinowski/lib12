806323c820dd8176746930ca6d1542d997d73faa
Krzysztof Kalinowski <kkalinowski@outlook.com>
2018-10-17 21:35:02 +0200

    Working on PropertyOrderComparer

--- lib12.Tests/Utility/PropertyOrderComparerTests.cs
+++ lib12.Tests/Utility/PropertyOrderComparerTests.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Linq;
+using lib12.Collections.Packing;
 using lib12.Utility;
 using Shouldly;
 using Xunit;
@@ -18,8 +20,9 @@ namespace lib12.Tests.Utility
         {
             const string string1 = "lorem xyz";
             const string string2 = "ipsum";
-            var comparer = new PropertyOrderComparer<string, int>(x => x.Length);
+            var comparer = new PropertyOrderComparer<string>(x => x.Length);
 
+            Factory.For<string>(x => x.Length);
             comparer.Compare(string1, string2).ShouldBe(1);
         }
 
@@ -35,19 +38,26 @@ namespace lib12.Tests.Utility
                 Nested = new CompareTarget { Value = 12 }
             };
 
-            var comparer = new PropertyOrderComparer<CompareTarget, CompareTarget>(x => x.Nested);
+            var comparer = new PropertyOrderComparer<CompareTarget>(x => x.Nested);
 
             Assert.Throws<ArgumentException>(() => comparer.Compare(obj1, obj2));
         }
 
         [Fact]
-        public void compare_two_string_by_length_using_get_hash_code()
+        public void PropertyOrderComparer_can_be_used_to_order_collection()
         {
-            const string string1 = "lorem";
-            const string string2 = "ipsum";
-            var comparer = new PropertyEqualityComparer<string>(x => x.Length);
+            var string1 = "winter is coming";
+            var string2 = "abc";
+            var string3 = "car-auto";
+            var unordered = Pack.IntoArray(string1, string2, string3);
+            
+            var ordered = unordered
+                .OrderBy(x => x, Factory.For<string>(x => x.Length))
+                .ToArray();
 
-            Assert.Equal(comparer.GetHashCode(string1), comparer.GetHashCode(string2));
+            ordered[0].ShouldBe(string2);
+            ordered[1].ShouldBe(string3);
+            ordered[2].ShouldBe(string1);
         }
     }
 }
--- lib12/Utility/PropertyOrderComparer.cs
+++ lib12/Utility/PropertyOrderComparer.cs
@@ -7,16 +7,15 @@ namespace lib12.Utility
     /// Compares two objects for order based on single property value
     /// </summary>
     /// <typeparam name="TSource"></typeparam>
-    /// <typeparam name="TKey"></typeparam>
-    public class PropertyOrderComparer<TSource, TKey> : IComparer<TSource> where TSource : class
+    public class PropertyOrderComparer<TSource> : IComparer<TSource> where TSource : class
     {
-        private readonly Func<TSource, TKey> _selector;
+        private readonly Func<TSource, object> _selector;
 
         /// <summary>
-        /// Initializes a new instance of the <see cref="PropertyOrderComparer{TSource, TKey}"/> class.
+        /// Initializes a new instance of the <see cref="PropertyOrderComparer{TSource}"/> class.
         /// </summary>
         /// <param name="selector">The key extractor.</param>
-        public PropertyOrderComparer(Func<TSource, TKey> selector)
+        public PropertyOrderComparer(Func<TSource, object> selector)
         {
             _selector = selector;
         }
@@ -27,27 +26,27 @@ namespace lib12.Utility
         /// <typeparam name="TSource">Type of object to compare</typeparam>
         /// <param name="selector">Selector of value to compare</param>
         /// <returns></returns>
-        public static PropertyOrderComparer<TSource, TKey> For<TSource, TKey>(Func<TSource, TKey> selector) where TSource : class
+        public static PropertyOrderComparer<TSource> For<TSource>(Func<TSource, object> selector) where TSource : class
         {
             if (selector == null)
                 throw new ArgumentNullException(nameof(selector));
 
-            return new PropertyOrderComparer<TSource, TKey>(selector);
+            return new PropertyOrderComparer<TSource>(selector);
         }
 
         /// <inheritdoc />
         public int Compare(TSource x, TSource y)
         {
-            return Comparer<TKey>.Default.Compare(_selector(x), _selector(y));
+            return Comparer<object>.Default.Compare(_selector(x), _selector(y));
         }
     }
 
     public class Factory
     {
-        public static PropertyOrderComparer<TSource, TKey> For<TSource, TKey>(Func<TSource, TKey> selector)
+        public static IComparer<TSource> For<TSource>(Func<TSource, object> selector)
             where TSource : class
         {
-            return new PropertyOrderComparer<TSource, TKey>(selector);
+            return new PropertyOrderComparer<TSource>(selector);
         }
     }
 }
