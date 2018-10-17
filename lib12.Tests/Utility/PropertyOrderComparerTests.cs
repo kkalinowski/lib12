a523f6c412ae8a2ad52a03cc5c03928f5220036d
Krzysztof Kalinowski <kkalinowski@outlook.com>
2018-10-17 19:14:38 +0200

    Working on PropertyOrderComparer

---
+++ lib12.Tests/Utility/PropertyOrderComparerTests.cs
@@ -0,0 +1,28 @@
+﻿using lib12.Utility;
+using Xunit;
+
+namespace lib12.Tests.Utility
+{
+    public class PropertyOrderComparerTests
+    {
+        [Fact]
+        public void compare_two_string_by_length()
+        {
+            const string string1 = "lorem";
+            const string string2 = "ipsum";
+            var comparer = PropertyOrderComparer<string, int>.For<string>(x => x.Length);
+
+            //Assert.True(comparer.Equals(string1, string2));
+        }
+
+        [Fact]
+        public void compare_two_string_by_length_using_get_hash_code()
+        {
+            const string string1 = "lorem";
+            const string string2 = "ipsum";
+            var comparer = new PropertyEqualityComparer<string>(x => x.Length);
+
+            Assert.Equal(comparer.GetHashCode(string1), comparer.GetHashCode(string2));
+        }
+    }
+}
--- lib12/Utility/PropertyOrderComparer.cs
+++ lib12/Utility/PropertyOrderComparer.cs
@@ -4,26 +4,41 @@ using System.Collections.Generic;
 namespace lib12.Utility
 {
     /// <summary>
-    /// Compares two objects for equality based on single property value
+    /// Compares two objects for order based on single property value
     /// </summary>
-    /// <typeparam name="T"></typeparam>
-    public class PropertyOrderComparer<T> : IComparer<T> where T : class
+    /// <typeparam name="TSource"></typeparam>
+    /// <typeparam name="TKey"></typeparam>
+    public class PropertyOrderComparer<TSource, TKey> : IComparer<TSource> where TSource : class
     {
-        private readonly Func<T, object> _keyExtractor;
+        private readonly Func<TSource, TKey> _selector;
 
         /// <summary>
-        /// Initializes a new instance of the <see cref="PropertyOrderComparer{T}"/> class.
+        /// Initializes a new instance of the <see cref="PropertyOrderComparer{TSource, TKey}"/> class.
         /// </summary>
-        /// <param name="keyExtractor">The key extractor.</param>
-        public PropertyOrderComparer(Func<T, object> keyExtractor)
+        /// <param name="selector">The key extractor.</param>
+        public PropertyOrderComparer(Func<TSource, TKey> selector)
         {
-            _keyExtractor = keyExtractor;
+            _selector = selector;
+        }
+
+        /// <summary>
+        /// Creates order comparer for given type using selector
+        /// </summary>
+        /// <typeparam name="TSource">Type of object to compare</typeparam>
+        /// <param name="selector">Selector of value to compare</param>
+        /// <returns></returns>
+        public static PropertyOrderComparer<TSource, TKey> For<TSource>(Func<TSource, TKey> selector) where TSource : class
+        {
+            if (selector == null)
+                throw new ArgumentNullException(nameof(selector));
+
+            return new PropertyOrderComparer<TSource, TKey>(selector);
         }
 
         /// <inheritdoc />
-        public int Compare(T x, T y)
+        public int Compare(TSource x, TSource y)
         {
-            return Comparer<T>.Default.Compare(x, y);
+            return Comparer<TKey>.Default.Compare(_selector(x), _selector(y));
         }
     }
 }
