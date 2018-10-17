c540a5ee55c3f6368574548ee5c79dd9473fa69c
Krzysztof Kalinowski <kkalinowski@outlook.com>
2018-10-17 21:18:55 +0200

    Added unit tests for PropertyOrderComparer

--- lib12.Tests/Utility/PropertyOrderComparerTests.cs
+++ lib12.Tests/Utility/PropertyOrderComparerTests.cs
@@ -1,20 +1,43 @@
-﻿using lib12.Utility;
+﻿using System;
+using lib12.Utility;
+using Shouldly;
 using Xunit;
 
 namespace lib12.Tests.Utility
 {
     public class PropertyOrderComparerTests
     {
+        private class CompareTarget
+        {
+            public int Value { get; set; }
+            public CompareTarget Nested { get; set; }
+        }
+
         [Fact]
         public void compare_two_string_by_length()
         {
-            const string string1 = "lorem";
+            const string string1 = "lorem xyz";
             const string string2 = "ipsum";
-            var comparer = PropertyOrderComparer<string, int>.For<string, int>(x => x.Length);
-            var comparer2 = Factory.For<string, int>(x => x.Length);
-            var comparer3 = new PropertyOrderComparer<string, int>(x=>x.Length);
+            var comparer = new PropertyOrderComparer<string, int>(x => x.Length);
 
-            //Assert.True(comparer.Equals(string1, string2));
+            comparer.Compare(string1, string2).ShouldBe(1);
+        }
+
+        [Fact]
+        public void compare_two_objects_without_not_implementing_comparable_throws_exception()
+        {
+            var obj1 = new CompareTarget
+            {
+                Nested = new CompareTarget { Value = 3 }
+            };
+            var obj2 = new CompareTarget
+            {
+                Nested = new CompareTarget { Value = 12 }
+            };
+
+            var comparer = new PropertyOrderComparer<CompareTarget, CompareTarget>(x => x.Nested);
+
+            Assert.Throws<ArgumentException>(() => comparer.Compare(obj1, obj2));
         }
 
         [Fact]
