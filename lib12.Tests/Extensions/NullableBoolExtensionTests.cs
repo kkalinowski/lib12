24685b9de40a328d387af76bd402d51a9ef1cae6
Krzysztof Kalinowski <kkalinowski@outlook.com>
2018-10-18 16:48:43 +0200

    Added NullableBoolExtension

---
+++ lib12.Tests/Extensions/NullableBoolExtensionTests.cs
@@ -0,0 +1,36 @@
+﻿using lib12.Extensions;
+using Shouldly;
+using Xunit;
+
+namespace lib12.Tests.Extensions
+{
+    public class NullableBoolExtensionTests
+    {
+        [Theory]
+        [InlineData(null, false)]
+        [InlineData(false, false)]
+        [InlineData(true, true)]
+        public void IsTrue_is_correct(bool? source, bool expectedResult)
+        {
+            source.IsTrue().ShouldBe(expectedResult);
+        }
+
+        [Theory]
+        [InlineData(null, false)]
+        [InlineData(false, true)]
+        [InlineData(true, false)]
+        public void IsFalse_is_correct(bool? source, bool expectedResult)
+        {
+            source.IsFalse().ShouldBe(expectedResult);
+        }
+
+        [Theory]
+        [InlineData(null, true)]
+        [InlineData(false, true)]
+        [InlineData(true, false)]
+        public void IsNullOrFalse_is_correct(bool? source, bool expectedResult)
+        {
+            source.IsNullOrFalse().ShouldBe(expectedResult);
+        }
+    }
+}
---
+++ lib12/Extensions/NullableBoolExtension.cs
@@ -0,0 +1,20 @@
+﻿namespace lib12.Extensions
+{
+    public static class NullableBoolExtension
+    {
+        public static bool IsTrue(this bool? source)
+        {
+            return source.HasValue && source.Value;
+        }
+
+        public static bool IsFalse(this bool? source)
+        {
+            return source.HasValue && !source.Value;
+        }
+
+        public static bool IsNullOrFalse(this bool? source)
+        {
+            return !source.HasValue || (source.HasValue && !source.Value);
+        }
+    }
+}
