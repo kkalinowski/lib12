3933969110e6ba6501f12b4f15dce9613d83348e
Krzysztof Kalinowski <kkalinowski@outlook.com>
2018-10-20 14:45:04 +0200

    File cleaning

--- lib12/Extensions/DateTimeExtension.cs
+++ lib12/Extensions/DateTimeExtension.cs
@@ -90,6 +90,50 @@ namespace lib12.Extensions
         }
 
         /// <summary>
+        /// Get the start of week from given date. Takes time into account.
+        /// </summary>
+        /// <param name="source">The source datetime</param>
+        /// <returns></returns>
+        public static DateTime GetStartOfWeek(this DateTime source)
+        {
+            //credits to https://stackoverflow.com/a/38064/578560
+            var diff = (7 + (source.DayOfWeek - DayOfWeek.Monday)) % 7;
+            return source.AddDays(-1 * diff).Date;
+        }
+
+        /// <summary>
+        /// Get the end of week from given date. Takes time into account.
+        /// </summary>
+        /// <param name="source">The source datetime</param>
+        /// <returns></returns>
+        public static DateTime GetEndOfWeek(this DateTime source)
+        {
+            var lastDay = source.GetStartOfWeek().AddDays(6);
+            return new DateTime(lastDay.Year, lastDay.Month, lastDay.Day, 23, 59, 59);
+        }
+
+        /// <summary>
+        /// Get the start of month from given date. Takes time into account.
+        /// </summary>
+        /// <param name="source">The source datetime</param>
+        /// <returns></returns>
+        public static DateTime GetStartOfMonth(this DateTime source)
+        {
+            return new DateTime(source.Year, source.Month, 1);
+        }
+
+        /// <summary>
+        /// Get the end of month from given date. Takes time into account.
+        /// </summary>
+        /// <param name="source">The source datetime</param>
+        /// <returns></returns>
+        public static DateTime GetEndOfMonth(this DateTime source)
+        {
+            var lastDayInMonth = DateTime.DaysInMonth(source.Year, source.Month);
+            return new DateTime(source.Year, source.Month, lastDayInMonth, 23, 59, 59);
+        }
+
+        /// <summary>
         /// Checks if given datetime contains past datetime
         /// </summary>
         /// <param name="source">The source datetime</param>
@@ -135,50 +179,6 @@ namespace lib12.Extensions
         public static bool IsWeekend(this DateTime source)
         {
             return source.DayOfWeek.IsAnyOf(DayOfWeek.Saturday, DayOfWeek.Sunday);
-        }
-
-        /// <summary>
-        /// Get the start of week from given date. Takes time into account.
-        /// </summary>
-        /// <param name="source">The source datetime</param>
-        /// <returns></returns>
-        public static DateTime GetStartOfWeek(this DateTime source)
-        {
-            //credits to https://stackoverflow.com/a/38064/578560
-            var diff = (7 + (source.DayOfWeek - DayOfWeek.Monday)) % 7;
-            return source.AddDays(-1 * diff).Date;
-        }
-
-        /// <summary>
-        /// Get the end of week from given date. Takes time into account.
-        /// </summary>
-        /// <param name="source">The source datetime</param>
-        /// <returns></returns>
-        public static DateTime GetEndOfWeek(this DateTime source)
-        {
-            var lastDay = source.GetStartOfWeek().AddDays(6);
-            return new DateTime(lastDay.Year, lastDay.Month, lastDay.Day, 23, 59, 59);
-        }
-
-        /// <summary>
-        /// Get the start of month from given date. Takes time into account.
-        /// </summary>
-        /// <param name="source">The source datetime</param>
-        /// <returns></returns>
-        public static DateTime GetStartOfMonth(this DateTime source)
-        {
-            return new DateTime(source.Year, source.Month, 1);
-        }
-
-        /// <summary>
-        /// Get the end of month from given date. Takes time into account.
-        /// </summary>
-        /// <param name="source">The source datetime</param>
-        /// <returns></returns>
-        public static DateTime GetEndOfMonth(this DateTime source)
-        {
-            var lastDayInMonth = DateTime.DaysInMonth(source.Year, source.Month);
-            return new DateTime(source.Year, source.Month, lastDayInMonth, 23, 59, 59);
         }
     }
 }
