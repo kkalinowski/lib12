115385df6912adbf58ff6d24a1fc227be638db6b
Krzysztof Kalinowski <kkalinowski@outlook.com>
2018-11-20 16:08:54 +0100

    Added ExceptionExtension

---
+++ lib12/Extensions/ExceptionExtension.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace lib12.Extensions
+{
+    public static class ExceptionExtension
+    {
+        public static Exception GetMostInnerException(this Exception exception)
+        {
+            if (exception.InnerException == null)
+                return exception;
+
+            return exception
+                .GetInnerExceptions()
+                .Last();
+        }
+
+        public static IEnumerable<Exception> GetInnerExceptions(this Exception exception)
+        {
+            var ex = exception.InnerException;
+            while (ex != null)
+            {
+                yield return ex;
+                ex = ex.InnerException;
+            }
+        }
+    }
+}
