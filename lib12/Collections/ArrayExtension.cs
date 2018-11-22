0ff936b7ec6d5a0bd0f1ea807c8f4cf3e3b4f42e
Krzysztof Kalinowski <kkalinowski@outlook.com>
2018-11-22 22:34:35 +0100

    Added methods for array flattening

---
+++ lib12/Collections/ArrayExtension.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace lib12.Collections
+{
+    /// <summary>
+    /// ArrayExtension
+    /// </summary>
+    public static class ArrayExtension
+    {
+        /// <summary>
+        /// Flattens 2D array to one dimensional enumerable
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="source">The source array</param>
+        /// <returns></returns>
+        public static IEnumerable<T> Flatten<T>(this T[,] source)
+        {
+            if (source == null)
+                yield break;
+
+            foreach (var item in source)
+                yield return item;
+        }
+
+        /// <summary>
+        /// Flattens 2D jagged array to one dimensional enumerable
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="source">The source array</param>
+        /// <returns></returns>
+        public static IEnumerable<T> Flatten<T>(this T[][] source)
+        {
+            return source
+                .Recover()
+                .SelectMany(x => x);
+        }
+
+        /// <summary>
+        /// Flattens 3D array to one dimensional enumerable
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="source">The source array</param>
+        /// <returns></returns>
+        public static IEnumerable<T> Flatten<T>(this T[,,] source)
+        {
+            if (source == null)
+                yield break;
+
+            foreach (var item in source)
+                yield return item;
+        }
+
+        /// <summary>
+        /// Flattens 2D jagged array to one dimensional enumerable
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="source">The source array</param>
+        /// <returns></returns>
+        public static IEnumerable<T> Flatten<T>(this T[][][] source)
+        {
+            return source
+                .Recover()
+                .SelectMany(x => x)
+                .SelectMany(x => x);
+        }
+    }
+}
