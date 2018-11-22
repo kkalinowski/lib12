fd1355906106e1fc7099966e2bc0d2bc7aa4b3c6
Krzysztof Kalinowski <kkalinowski@outlook.com>
2018-11-22 22:10:26 +0100

    xdoc for ExceptionExtension

--- lib12/Extensions/ExceptionExtension.cs
+++ lib12/Extensions/ExceptionExtension.cs
@@ -4,8 +4,17 @@ using System.Linq;
 
 namespace lib12.Extensions
 {
+    /// <summary>
+    /// ExceptionExtension
+    /// </summary>
     public static class ExceptionExtension
     {
+        /// <summary>
+        /// Gets the most inner exception of given exception.
+        /// If exception doesn't have InnerException returns source exception.
+        /// </summary>
+        /// <param name="exception">The source exception.</param>
+        /// <returns></returns>
         public static Exception GetMostInnerException(this Exception exception)
         {
             if (exception.InnerException == null)
@@ -16,6 +25,11 @@ namespace lib12.Extensions
                 .Last();
         }
 
+        /// <summary>
+        /// Gets list of inner exceptions from source exception
+        /// </summary>
+        /// <param name="exception">The source exception.</param>
+        /// <returns></returns>
         public static IEnumerable<Exception> GetInnerExceptions(this Exception exception)
         {
             var ex = exception.InnerException;
