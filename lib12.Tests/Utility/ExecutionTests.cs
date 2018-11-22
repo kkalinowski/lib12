b1d8e92ea9619f7f663858bd69c949e7b6a78e38
Krzysztof Kalinowski <kkalinowski@outlook.com>
2018-11-22 22:54:21 +0100

    Added Execution class with Repeat method

---
+++ lib12.Tests/Utility/ExecutionTests.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using lib12.Utility;
+using Shouldly;
+using Xunit;
+
+namespace lib12.Tests.Utility
+{
+    public class ExecutionTests
+    {
+        [Fact]
+        public void Repeat_is_correct()
+        {
+            var i = 0;
+            const int count = 5;
+
+            Execution.Repeat(count, () => i++);
+
+            i.ShouldBe(count);
+        }
+
+        [Fact]
+        public void Repeat_is_not_called_when_it_should_be_called_less_than_one_time()
+        {
+            var i = 0;
+
+            Execution.Repeat(0, () => i++);
+            Execution.Repeat(-5, () => i++);
+
+            i.ShouldBe(0);
+        }
+
+        [Fact]
+        public void Repeat_throws_exception_if_action_is_null()
+        {
+            Assert.Throws<ArgumentNullException>(() => Execution.Repeat(12, null));
+        }
+    }
+}
---
+++ lib12/Utility/Execution.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace lib12.Utility
+{
+    /// <summary>
+    /// Utility class for calling methods with additional effects.
+    /// </summary>
+    public static class Execution
+    {
+        /// <summary>
+        /// Repeats calls specified amount of times.
+        /// If you pass zero or negative number of calls, action isn't called at all.
+        /// </summary>
+        /// <param name="times">Number of times to call method</param>
+        /// <param name="action">The action to call</param>
+        /// <exception cref="ArgumentNullException">action</exception>
+        public static void Repeat(int times, Action action)
+        {
+            if (action == null)
+                throw new ArgumentNullException(nameof(action));
+
+            if (times < 1)
+                return;
+
+            for (var i = 0; i < times; i++)
+                action();
+        }
+    }
+}
