4822245aefdbaf99be4bed1d2068f2489c5aaa3d
Krzysztof Kalinowski <kkalinowski@outlook.com>
2019-01-16 18:38:25 +0100

    Working on memoization

---
+++ lib12/Utility/Memoizer.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace lib12.Utility
+{
+    public static class Memoizer
+    {
+        public static Func<TParam1, TResult> CreateForOneParameter<TParam1, TResult>(this Func<TParam1, TResult> func)
+        {
+            var map = new Dictionary<TParam1, TResult>();
+            return param1 =>
+            {
+                if (map.TryGetValue(param1, out var value))
+                    return value;
+
+                value = func(param1);
+                map.Add(param1, value);
+
+                return value;
+            };
+        }
+    }
+}
