5f718cfdc63f08de2dc3c41c471986cc08840e13
Krzysztof Kalinowski <kkalinowski@outlook.com>
2019-12-22 21:34:53 +0100

    Added country properties

--- lib12/Data/Geopolitical/Country.cs
+++ lib12/Data/Geopolitical/Country.cs
@@ -7,5 +7,20 @@ namespace lib12.Data.Geopolitical
     public class Country
     {
         public string Name { get; set; }
+        public string OfficialName { get; }
+        public double Latitude { get; }
+        public double Longitude { get; }
+        public string TopLevelDomainCode { get; }
+        public string Capital { get; }
+        public string Region { get; }
+        public string Subregion { get; }
+        public string[] Languages { get; }
+        public string NameOfResidents { get; }
+        public string EmojiFlag { get; }
+        public string IsoAlfa2Code { get; }
+        public string IsoAlfa3Code { get; }
+        public string IsoNumeric { get; }
+        public string[] Currencies { get; }
+        public string[] DialingPrefix { get; }
     }
 }
