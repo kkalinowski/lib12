684f2fb863837ca9f91547ab7c1ae6a9484ef38d
Krzysztof Kalinowski <kkalinowski@outlook.com>
2019-12-28 13:08:09 +0100

    Single countries in SingleCountries

--- lib12.CountryGenerator/Program.cs
+++ lib12.CountryGenerator/Program.cs
@@ -20,7 +20,7 @@ namespace lib12.CountryGenerator
         private const string UrlToCountryFile = "https://github.com/mledoze/countries/raw/master/countries.json";
         private const string CountryFilename = "countries.json";
         private const string CountryRepositoryFilename = @"..\..\..\..\lib12\Data\Geopolitical\CountryRepository.cs";
-        private const string CountryClassText = "        public static Country {0} {{ get; }} = new Country (\"{1}\", \"{2}\", {3}, {4}, \"{5}\", \"{6}\", \"{7}\",\"{8}\", {9}, \"{10}\", \"{11}\", \"{12}\", \"{13}\", \"{14}\", {15}, \"{16}\");\n";
+        private const string CountryClassText = "            public static Country {0} {{ get; }} = new Country (\"{1}\", \"{2}\", {3}, {4}, \"{5}\", \"{6}\", \"{7}\",\"{8}\", {9}, \"{10}\", \"{11}\", \"{12}\", \"{13}\", \"{14}\", {15}, \"{16}\");\n";
 
         static void Main(string[] args)
         {
@@ -74,7 +74,7 @@ namespace lib12.CountryGenerator
 
         private static void SaveHeaderOfFile(StringBuilder countryRepositoryBuilder)
         {
-            countryRepositoryBuilder.Append("#pragma warning disable 1591\r\nnamespace lib12.Data.Geopolitical\r\n{\r\n    public static class CountryRepository\r\n    {\n");
+            countryRepositoryBuilder.Append("#pragma warning disable 1591\r\n\nnamespace lib12.Data.Geopolitical\r\n{\r\n    public static class CountryRepository\r\n    {\n        public static class SingleCountries\r\n        {\n");
         }
 
         private static void SaveCountry(dynamic country, StringBuilder countryRepositoryBuilder)
@@ -142,7 +142,8 @@ namespace lib12.CountryGenerator
 
         private static void SaveEndOfFile(StringBuilder countryRepositoryBuilder)
         {
-            countryRepositoryBuilder.Append("     }\r\n}");
+            countryRepositoryBuilder.Remove(countryRepositoryBuilder.Length - 2, 2);
+            countryRepositoryBuilder.Append("        }\r\n    }\r\n}");
         }
     }
 }
